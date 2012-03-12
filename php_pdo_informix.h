78a21a309ec644ede1a22b7ba513f7205df91421
Rahul Priyadarshi <rahulpriyadarshi@php.net>
2012-03-12 11:19:51 +0000

    Version change for pdo_informix-1.2.7 release

--- php_pdo_informix.h
+++ php_pdo_informix.h
@@ -14,15 +14,15 @@
   | implied. See the License for the specific language governing         |
   | permissions and limitations under the License.                       |
   +----------------------------------------------------------------------+
-  | Authors: Rick McGuire, Dan Scott, Krishna Raman, Kellen Bombardier   |
-  |                                                                      |
+  | Authors: Rick McGuire, Dan Scott, Krishna Raman, Kellen Bombardier,  |
+  | Ambrish Bhargava                                                     |
   +----------------------------------------------------------------------+
 */
 
 #ifndef PHP_PDO_INFORMIX_H
 #define PHP_PDO_INFORMIX_H
 
-#define PDO_INFORMIX_VERSION "1.2.6"
+#define PDO_INFORMIX_VERSION "1.2.7"
 
 extern zend_module_entry pdo_informix_module_entry;
 #define phpext_pdo_informix_ptr &pdo_informix_module_entry
